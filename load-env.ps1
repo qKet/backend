2b42276da6b4e1a6329f99e8d5d3b670f624c8e0
iChaeYeong <147784687+iChaeYeong@users.noreply.github.com>
2026-08-06 16:05:19 +0900

    Delete load-env.ps1

--- load-env.ps1
+++
@@ -1,5 +0,0 @@
-Get-Content (Join-Path $PSScriptRoot ".env") | ForEach-Object {
-    if ($_ -match '^\s*([^#=\s][^=]*)\s*=\s*(.*)\s*$') {
-        Set-Item -Path "Env:$($matches[1].Trim())" -Value $matches[2].Trim()
-    }
-}
